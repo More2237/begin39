179cc6c81ea1a96b5656025565f09c9bf03a46a2
More2237 <noorallyrahmonov2308@gmail.com>
2024-11-29 09:44:42 +0500

    nbv

---
+++ begin39/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Consol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+
+            double d = b * b - 4 * a * c;
+
+            double sqrtd = Math.Sqrt(d);
+            
+            double x1 = b*b - 4*a * c;
+            double x2 = b*b + 4*a * c;
+            double result = Math.Min(x1, x2);
+            double result2 = Math.Max(x1, x2);
+            Console.WriteLine(result);
+            Console.WriteLine(result2);
+            
+            
+        }
+    }
+}
